b5709cd7a0023abe6f51955eb4d665491270211d
Gregorius Vincent Adam <55656497+GregoriusVincentAdam@users.noreply.github.com>
2022-11-27 15:51:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+/*  
+Nama Kelompok:  Denver
+Nama Anggota :  Adam, Gregorius Vincent
+                Luntungan, Hendra Ucok
+                Lahengke, Gabriela
+                Kalalo, Sherren Brenda
+                Tangkeallo, Yazel Stepano
+                Lobbu, Stephen Anthony Timothy
+*/
+
+import React, {Component} from "react";
+
+export default class StateProps extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            makanan : "Migori"
+        }
+    }
+    render(){
+        return (
+            <div>
+                <h1>{this.state.makanan}</h1>
+                <button onClick={()=>this.setState({makanan: "Nasi Goreng"})}> Change Your Fav Food </button>
+            </div>
+        )
+    }
+}
