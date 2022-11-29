31320ae0862cba96ab4009841578a70f294181da
SherrenKalalo <s2200043@student.unklab.ac.id>
2022-11-29 15:39:10 +0800

    Ubah bagian boks deskripsi

--- restaurant-app/src/components/Homepage.js
+++ restaurant-app/src/components/Homepage.js
@@ -65,10 +65,11 @@ const Homepage = () => {
                     </div>
                     <div className="col-md-6">
                     <div className="tm-description-box"> 
-                        <h4 className="tm-gallery-title">Terbukti Terkenal!!!</h4>
+                    <br></br>
+                        <h2 style={{color: 'rgba(110, 1, 1, 1)'}}>Terbukti Terkenal!!!</h2> <br></br>
                         <p className="tm-mb-45">See??? Bahkan Jess No Limit dan Sisca Kohl pernah makang disiniii :p. So, tunggu apalagi guys? Pesan jo sekarang! ^^</p>
                         <a>Mo suka lia sapa-sapa leh yang pernah makang disini? Coba klik di tombol "Lia laeng" di bawah :D</a>
-                        <br></br><br></br><br></br><br></br><br></br><a href="about.html" className="tm-btn tm-btn-default tm-right">Lia laeng</a>
+                        <br></br><br></br><br></br><a href="about.html" className="tm-btn tm-btn-default tm-right" style={{color: 'rgba(110, 0, 0, 1)'}}>Lia laeng</a>
                     </div>
                     </div>
                 </div>
